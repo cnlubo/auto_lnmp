15135d8bb49d41756807d08c34ec39050361bb19
卢波 <lubo@91anshi.com>
2017-06-14 17:40:44 +0800

    update mysql5.7

--- function/install/Mysql-5.7.sh
+++ function/install/Mysql-5.7.sh
@@ -247,7 +247,7 @@ INIT_MySQL_DB(){
 
 MysqlDB_Install_Main(){
 
-    MySQL_Var&&Create_Conf&&INIT_MySQL_DB
+    MySQL_Var&&MYSQL_BASE_PACKAGES_INSTALL&&Create_Conf&&INIT_MySQL_DB
     #&&MYSQL_BASE_PACKAGES_INSTALL&&INSTALL_MysqlDB&&Create_Conf
 
 
