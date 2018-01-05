09b5a6777a5438da3f8dd9adf623160c88538285
cnlubo <bolu20080314@gmail.com>
2018-01-05 14:10:39 +0800

    update

--- include/check_deps.sh
+++ include/check_deps.sh
@@ -9,7 +9,8 @@ installDepsCentOS() {
 
     #sed -i 's@^exclude@#exclude@' /etc/yum.conf #注释exclude
     echo "${CMSG} install yum-fastestmirror epel-release...${CEND}"
-    yum -y install yum-fastestmirror epel-release
+    # yum -y install yum-fastestmirror epel-release
+    yum -y install epel-release
     yum clean all
     yum makecache
 
@@ -226,7 +227,7 @@ installDepsBySrc() {
             U_V3=`python -V 2>&1|awk '{print $2}'|awk -F '.' '{print $3}'`
             Python_version=$U_V1.$U_V2.$U_V3
         fi
-        if [ $Python_version! = ${python2_version:?} ] || [ ! -e "$( which python )" ]; then
+        if [ $Python_version != ${python2_version:?} ] || [ ! -e "$( which python )" ]; then
             cd $script_dir/src
             echo
             echo "${CMSG}****************** Python-$python2_version install begin *************************************>>${CEND}"
