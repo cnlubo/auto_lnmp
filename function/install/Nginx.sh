14ecaa4226645766a2903e1b6033f09f834c9fe9
cnlubo <bolu20080314@gmail.com>
2018-01-25 14:07:06 +0800

    update

--- function/install/Nginx.sh
+++ function/install/Nginx.sh
@@ -7,8 +7,8 @@
 #----------------------------------------------------------------------------
 Nginx_stable_install(){
 
-    echo "${CMSG}[nginx-${ngix_stable_version:?} install begin ]*************>>${CEND}\n"
-    echo "${CMSG}[step1 zlib pcre jemalloc install ]*************************>>${CEND}\n"
+    echo -e "${CMSG}[nginx-${ngix_stable_version:?} install begin ]*************>>${CEND}\n"
+    echo -e "${CMSG}[step1 zlib pcre jemalloc install ]*************************>>${CEND}\n"
 
 
 
