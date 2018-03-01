449fe9c99db3c53412994e7e628ca8f6412c511f
cnlubo <bolu20080314@gmail.com>
2018-03-01 18:05:03 +0800

    update

--- function/install/Tengine.sh
+++ function/install/Tengine.sh
@@ -102,7 +102,7 @@ Install_Tengine(){
     --http-uwsgi-temp-path=${tengine_install_dir:?}/tmp/uwsgi \
     --http-scgi-temp-path=${tengine_install_dir:?}/tmp/scgi \
     --with-openssl=${script_dir:?}/src/openssl-${openssl_version:?} --with-openssl-opt=-fPIC \
-    --with-pcre=${script_dir:?}/src/pcre-${pcre_version:?}--with-pcre-opt=-fPIC --with-pcre-jit \
+    --with-pcre=${script_dir:?}/src/pcre-${pcre_version:?} --with-pcre-opt=-fPIC --with-pcre-jit \
     --with-jemalloc \
     --with-zlib=${script_dir:?}/src/zlib-${zlib_version:?} $nginx_modules_options
 
