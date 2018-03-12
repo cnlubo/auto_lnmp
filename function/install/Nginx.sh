49def4336cce6785d310a49a73f253c41e6802af
cnlubo <bolu20080314@gmail.com>
2018-03-12 16:40:29 +0800

    commit

--- function/install/Nginx.sh
+++ function/install/Nginx.sh
@@ -40,7 +40,7 @@ Install_Nginx(){
     tar xf nginx-${nginx_install_version:?}.tar.gz
     cd nginx-${nginx_install_version:?}
     if [ ${lua_install:?} = 'y' ]; then
-        nginx_modules_options="--with-ld-opt='-Wl,-rpath,/usr/local/luajit/lib'"
+        nginx_modules_options="--with-ld-opt='-Wl,-rpath,/usr/local/luajit/lib -ljemalloc'"
         nginx_modules_options=$nginx_modules_options" --add-module=../ngx_devel_kit-${ngx_devel_kit_version:?}"
         nginx_modules_options=$nginx_modules_options" --add-module=../lua-nginx-module-${lua_nginx_module_version:?}"
         nginx_modules_options=$nginx_modules_options" --with-stream --with-stream_ssl_module"
@@ -48,7 +48,7 @@ Install_Nginx(){
         export LUAJIT_LIB=/usr/local/luajit/lib
         export LUAJIT_INC=/usr/local/luajit/include/luajit-2.1
     else
-        nginx_modules_options=''
+        nginx_modules_options='--with-ld-opt=-ljemalloc'
     fi
     ./configure --prefix=${nginx_install_dir:?} \
         --sbin-path=${nginx_install_dir:?}/sbin/nginx \
@@ -71,7 +71,7 @@ Install_Nginx(){
         --http-fastcgi-temp-path=${nginx_install_dir:?}/tmp/fcgi/ \
         --http-uwsgi-temp-path=${nginx_install_dir:?}/tmp/uwsgi \
         --http-scgi-temp-path=${nginx_install_dir:?}/tmp/scgi \
-        --with-ld-opt="-ljemalloc" --with-openssl=../openssl-${openssl_latest_version:?} \
+        --with-openssl=../openssl-${openssl_latest_version:?} \
         --with-pcre=../pcre-${pcre_version:?} --with-pcre-jit \
         --with-zlib=../zlib-${zlib_version:?} \
         --add-dynamic-module=../ngx_brotli \
