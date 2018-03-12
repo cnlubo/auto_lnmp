f9a524601e30eba0314f43b792eec321d4173009
cnlubo <bolu20080314@gmail.com>
2018-03-12 17:02:11 +0800

    commit

--- function/install/Nginx.sh
+++ function/install/Nginx.sh
@@ -40,11 +40,12 @@ Install_Nginx(){
     tar xf nginx-${nginx_install_version:?}.tar.gz
     cd nginx-${nginx_install_version:?}
     if [ ${lua_install:?} = 'y' ]; then
-        nginx_modules_options="--with-ld-opt=-Wl,-rpath,/usr/local/luajit/lib,-ljemalloc"
+        nginx_modules_options="--with-ld-opt=-Wl,-rpath,'/usr/local/luajit/lib',ljemalloc"
         nginx_modules_options=$nginx_modules_options" --add-module=../ngx_devel_kit-${ngx_devel_kit_version:?}"
         nginx_modules_options=$nginx_modules_options" --add-module=../lua-nginx-module-${lua_nginx_module_version:?}"
         nginx_modules_options=$nginx_modules_options" --with-stream --with-stream_ssl_module"
         nginx_modules_options=$nginx_modules_options" --add-module=../stream-lua-nginx-module"
+        echo $nginx_modules_options
         export LUAJIT_LIB=/usr/local/luajit/lib
         export LUAJIT_INC=/usr/local/luajit/include/luajit-2.1
     else
@@ -155,5 +156,5 @@ EOF
 
 Nginx_Install_Main() {
     Nginx_Var && Nginx_Base_Dep_Install && Nginx_Dep_Install && Install_Nginx
-    #&& Config_Nginx
+
 }
