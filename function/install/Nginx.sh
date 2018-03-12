a034170bc61c3579f472348880e700b427d10360
cnlubo <bolu20080314@gmail.com>
2018-03-12 14:58:55 +0800

    commit

--- function/install/Nginx.sh
+++ function/install/Nginx.sh
@@ -37,14 +37,14 @@ Install_Nginx(){
     src_url=https://nginx.org/download/nginx-${nginx_install_version:?}.tar.gz
     [ ! -f nginx-${nginx_install_version:?}.tar.gz ] && Download_src
     [ -d nginx-${nginx_install_version:?} ] && rm -rf nginx-${nginx_install_version:?}
-    tar xvf nginx-${nginx_install_version:?}.tar.gz
+    tar xf nginx-${nginx_install_version:?}.tar.gz
     cd nginx-${nginx_install_version:?}
     if [ ${lua_install:?} = 'y' ]; then
         nginx_modules_options="--with-ld-opt='-Wl,-rpath,/usr/local/luajit/lib'"
         nginx_modules_options=$nginx_modules_options" --add-module=../ngx_devel_kit-${ngx_devel_kit_version:?}"
         nginx_modules_options=$nginx_modules_options" --add-module=../lua-nginx-module-${lua_nginx_module_version:?}"
         nginx_modules_options=$nginx_modules_options" --with-stream=dynamic --with-stream_ssl_module"
-        nginx_modules_options=$nginx_modules_options" --add-module=../stream-lua-nginx-module"
+        nginx_modules_options=$nginx_modules_options" --add-dynamic-module=../stream-lua-nginx-module"
         export LUAJIT_LIB=/usr/local/luajit/lib
         export LUAJIT_INC=/usr/local/luajit/include/luajit-2.1
     else
