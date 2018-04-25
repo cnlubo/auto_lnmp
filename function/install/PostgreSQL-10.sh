cb28efeaee76a5f965c11937add3cdc44d6bcd5a
cnlubo <bolu20080314@gmail.com>
2018-04-25 11:34:23 +0800

    commit

--- function/install/PostgreSQL-10.sh
+++ function/install/PostgreSQL-10.sh
@@ -68,8 +68,8 @@ Init_PostgreSQL(){
     # 手工启动数据库
     sudo -u ${pgsql_user:?} -H ${PgsqlBasePath:?}/bin/pg_ctl -D ${PgsqlOptPath:?}/data -l ${PgsqlOptPath:?}/logs/alert.log start
     # set pgsql_user passwd
-    su - ${pgsql_user:?} -c "${PgsqlBasePath:?}/bin/psql -c \"alter ${pgsql_user:?} postgres with password '12345678';\" -d postgres"
-
+    su - ${pgsql_user:?} -c "${PgsqlBasePath:?}/bin/psql -h 127.0.0.1 -d postgres -c \"alter ${pgsql_user:?} postgres with password '12345678';\""
+    # su - ak47 -c "/u01/pgsql/10.3/bin/psql -h 127.0.0.1 -d postgres -c \"alter user ak47 with password '12345678'\""
 }
 
 
