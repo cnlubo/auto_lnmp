ef303fccc6f8dff7df76a3814ab3ca688b109938
cnlubo <bolu20080314@gmail.com>
2018-05-16 10:04:05 +0800

    committed

--- function/install/redmine_install.sh
+++ function/install/redmine_install.sh
@@ -39,6 +39,7 @@ passenger_install (){
         INFO_MSG "[Phusion Passenger is already installed ......]"
     else
         INFO_MSG "[ Phusion Passenger Installing ......]"
+        echo $PATH
         gem install passenger --no-ri --no-rdoc
         if [ -f /root/.zshrc ]; then
             echo export 'PATH=$PATH:'"${ruby_install_dir:?}/bin" >>/root/.zshrc
@@ -48,7 +49,7 @@ passenger_install (){
             SOURCE_SCRIPT /root/.bash_profile
         fi
         #export PATH=$PATH:${ruby_install_dir:?}/bin
-        echo $PATH
+        echo "new:"$PATH
         sed -i "s@^passenger_root.*@passenger_root=$(passenger-config --root)@" ${script_dir:?}/config/redmine.conf
         sed -i "s@^nginx_addon_dir.*@nginx_addon_dir=$(passenger-config --nginx-addon-dir)@" ${script_dir:?}/config/redmine.conf
         sed -i "s@^passenger_ruby.*@passenger_ruby=${ruby_install_dir:?}/bin/ruby@" ${script_dir:?}/config/redmine.conf
--- include/test_zsh.sh
+++ include/test_zsh.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-source /root/.zshrc
+. /root/.zshrc
