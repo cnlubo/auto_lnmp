dc77482f151a11dd2ac6dc82948d29fdd103fffd
cnlubo <bolu20080314@gmail.com>
2018-05-30 16:55:40 +0800

    committed

--- function/install/gitlab.sh
+++ function/install/gitlab.sh
@@ -239,8 +239,8 @@ Config_GitLab() {
         INFO_MSG "[Compile GetText PO files ......]"
         sudo -u git -H bundle exec rake gettext:compile RAILS_ENV=production
         INFO_MSG "[Compile Assets .....]"
-        #sudo -u git -H yarn install --production --pure-lockfile
-        #output1=`sudo -u git -H bundle exec rake yarn:install gitlab:assets:clean gitlab:assets:compile RAILS_ENV=production NODE_ENV=production`
+        sudo -u git -H yarn install --production --pure-lockfile
+        output1=`sudo -u git -H bundle exec rake yarn:install gitlab:assets:clean gitlab:assets:compile RAILS_ENV=production NODE_ENV=production`
         INFO_MSG "[Compile Assets finish .....]"
         INFO_MSG "[Fix Repo paths access ......]"
         chmod -R ug+rwX,o-rwx /home/git/repositories
@@ -262,7 +262,7 @@ Config_GitLab() {
 
 Gitlab_Install_Main() {
 
-    #GitLab_Var && GitLab_Dep_Install && Install_GitLab && 
+    #GitLab_Var && GitLab_Dep_Install && Install_GitLab &&
     Config_GitLab
 
 }
