ae0fef4691e9f27940a57e34f45de956f81b0a32
cnlubo <bolu20080314@gmail.com>
2018-06-08 11:54:38 +0800

    committed

--- include/docker.sh
+++ include/docker.sh
@@ -9,7 +9,7 @@
 Install_Docker() {
 
     INFO_MSG "[ docker-compose V${docker_compose_version:?} install ......]"
-    [ -f /usr/local/bin/docker-compose ] && mv usr/local/bin/docker-compose /usr/local/bin/docker-compose.bak
+    [ -f /usr/local/bin/docker-compose ] && mv /usr/local/bin/docker-compose /usr/local/bin/docker-compose.bak
     cd ${script_dir:?}/src
     curl -L https://github.com/docker/compose/releases/download/${docker_compose_version:?}/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
