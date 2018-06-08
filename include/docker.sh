fae7136a4606d7523ebd7bd95019fd5132fd4af1
cnlubo <bolu20080314@gmail.com>
2018-06-08 14:13:44 +0800

    committed

--- include/docker.sh
+++ include/docker.sh
@@ -8,12 +8,29 @@
 
 Install_Docker() {
 
+    INFO_MSG "[Uninstall old versions Docker ......]"
+    yum remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+    INFO_MSG "[ Docker CE install ......]"
+    yum install -y yum-utils device-mapper-persistent-data lvm2
+    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    # Enable the edge and test repositories
+    # yum-config-manager --enable docker-ce-edge
+    # yum-config-manager --enable docker-ce-test
     INFO_MSG "[ docker-compose V${docker_compose_version:?} install ......]"
     [ -f /usr/local/bin/docker-compose ] && mv /usr/local/bin/docker-compose /usr/local/bin/docker-compose.bak
     cd ${script_dir:?}/src
     curl -L https://github.com/docker/compose/releases/download/${docker_compose_version:?}/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
-    INFO_MSG "[ Install command completion for the bash and zsh shell ......]"
+    INFO_MSG "[ Install command completion for the bash or zsh shell ......]"
     if [ $SHELL = '/usr/local/bin/zsh' ] || [ $SHELL = '/bin/zsh' ]; then
         [ ! -d ~/.zsh/completion ] && mkdir -p ~/.zsh/completion
         curl -L https://raw.githubusercontent.com/docker/compose/${docker_compose_version:?}/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
@@ -21,8 +38,7 @@ Install_Docker() {
         echo 'autoload -Uz compinit && compinit -i' >> ~/.zshrc
         docker-compose --version
         # reload shell
-        exec $SHELL -l
-
+        #exec $SHELL -l
     fi
 
 
